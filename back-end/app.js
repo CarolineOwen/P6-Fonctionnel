2227c407f9bd997c9a327c35604e4a538e385810
Caroline Owen <carolinaowen95@gmail.com>
2022-06-27 11:41:59 +0200

    deuxieme commit

--- back-end/app.js
+++ back-end/app.js
@@ -1,31 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { addAbortSignal } = require('stream');
 const app = express();
+
+const stuffRoutes = require('./routes/stuff');
+
 mongoose.connect('mongodb+srv://Carolinadelavega1:vega2022.@cluster1.thg0nym.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie, toptip !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+  app.use(express.json());
 
-
-app.use((req, res, next)=>{
-console.log('Requete reçue');
-next();
-});
-
-app.use((req, res, next)=>{
-    res.status(201);
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
+  });
 
-app.use((req, res, next) => {
-    res.json({message:'votre requete fonctionne bien bien !'});
-    next();
-});
-
-app.use((req, res) => {
-    console.log('réponse succes');
-});
+ app.use('/api/sauces', stuffRoutes);
 
 module.exports= app;
