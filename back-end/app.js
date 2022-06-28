afb73c5c9a23390ea09eaa9986def686b9fbd06a
Caroline Owen <carolinaowen95@gmail.com>
2022-06-28 18:51:39 +0200

    ajout sauces

--- back-end/app.js
+++ back-end/app.js
@@ -1,31 +1,29 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const app = express();
 const mongoose = require('mongoose');
-//const { addAbortSignal } = require('stream');
+const path = require('path');
 
-
-const stuffRoutes = require('./routes/stuff');
+const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
 
+
 mongoose.connect('mongodb+srv://Carolinadelavega1:vega2022.@cluster1.thg0nym.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
+{ useNewUrlParser: true,
     useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie, toptip !'))
+  .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-  const app = express();
+app.use(express.json());
 
-  app.use((req, res, next) => {
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
 
-
- app.use(bodyParser.json()); 
- app.use('/api/sauces', stuffRoutes);
+app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-module.exports= app;
+module.exports = app;
---
+++ back-end/routes/sauces.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const auth = require('../middleware/auth');
+const router = express.Router();
+const multer = require('../middleware/multer-config');
+
+const saucesCtrl = require('../controllers/sauces');
+
+router.post('/', auth, multer, saucesCtrl.createSauce);
+
+  router.put('/:id', auth,  saucesCtrl.modifySauce);
+
+  router.delete('/:id', auth, saucesCtrl.deleteSauce );
+
+  router.get('/:id', auth, saucesCtrl.getOneSauce);
+
+router.get('/', auth, saucesCtrl.getAllSauces);
+
+
+module.exports = router;
